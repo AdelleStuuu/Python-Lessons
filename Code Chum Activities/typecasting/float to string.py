562ff1adee0bd2291ec148054cdfbec645915925
AdelleStuuu <adelledoesstuff@gmail.com>
2025-09-11 21:56:59 +0800

    I WASTED ALL MY COMMITS JUST BECUASE I WANTED TO MOVE THIS FOLDER

---
+++ Code Chum Activities/typecasting/float to string.py
@@ -0,0 +1,3 @@
+stringFloat = input('Enter a string containing a float: ')
+f = float(stringFloat)
+print(f'Floating-point number: {f:.2f}')
---
+++ Quiz Stuff/Pseudo to Code/Area Calc.py
@@ -0,0 +1,7 @@
+print()
+length = int(input('Enter value of Length: '))
+width = int(input('Enter value of Width: '))
+area = length * width
+print()
+print('The area is:',area)
+print()
