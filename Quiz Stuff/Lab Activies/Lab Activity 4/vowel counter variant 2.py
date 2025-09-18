629315d95ccda268bf4e6786933377b7e7bf70ac
AdelleStuuu~ <adelledoesstuff@gmail.com>
2025-09-18 20:47:42 +0800

    variant 2 tweaked to abide by codechum rules

--- Quiz Stuff/Lab Activies/Lab Activity 4/vowel counter variant 2.py
+++ Quiz Stuff/Lab Activies/Lab Activity 4/vowel counter variant 2.py
@@ -12,6 +12,8 @@ convertedPhrase = phrase.upper()
 for i in convertedPhrase:
     if i in vowels: 
         vowels[i] += 1
+    elif i not in vowels:
+        continue 
     
 for letter,amount in vowels.items():
     print(f"{letter} =  {amount}")
