f257e0d9dd820d903d96e55269b85aaee82d9be6
AdelleStuuu~ <adelledoesstuff@gmail.com>
2025-09-18 20:43:04 +0800

    added a second, shorter variant for the vowel counter

---
+++ Quiz Stuff/Lab Activies/Lab Activity 4/vowel counter variant 2.py
@@ -0,0 +1,19 @@
+vowels = {
+    'A' : 0,
+    'E' : 0,
+    'I' : 0,
+    'O' : 0,
+    'U' : 0,
+}
+
+phrase = input('Enter a phrase: ')
+convertedPhrase = phrase.upper()
+
+for i in convertedPhrase:
+    if i in vowels: 
+        vowels[i] += 1
+    
+for letter,amount in vowels.items():
+    print(f"{letter} =  {amount}")
+
+
