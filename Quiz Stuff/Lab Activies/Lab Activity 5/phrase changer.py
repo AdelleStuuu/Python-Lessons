f4be4cec3873cc76bc17c6e4cc59d65f77079bbb
AdelleStuuu~ <adelledoesstuff@gmail.com>
2025-10-16 14:51:49 +0800

    a phrase that changes on the given context, consonants are lowered, vowels are upper cased, spaces are turned to x

---
+++ Quiz Stuff/Lab Activies/Lab Activity 5/phrase changer.py
@@ -0,0 +1,17 @@
+vowel = "aeiou"
+
+phrase = input("Enter a sentence or phrase: ")
+print(f"Original string: {phrase}")
+
+for i in phrase: 
+    if i == " ":
+        phrase = phrase.replace(" ", "x")
+        
+    if i.lower() in vowel:
+        phrase = phrase.replace(i,i.upper())
+    else:
+        phrase = phrase.replace(i,i.lower())
+    
+    
+        
+print(f"Processed string: {phrase}")
