bce47afc527a875b4f90eed54c7eee81c137895f
AdelleStuuu~ <adelledoesstuff@gmail.com>
2025-10-16 14:51:02 +0800

    a program that uses match case, a function to compute and acts like a ticketing system

---
+++ Quiz Stuff/Lab Activies/Lab Activity 5/Movie Ticket.py
@@ -0,0 +1,34 @@
+print("==============================")
+print("   MOVIE TICKETING SYSTEM")
+print("==============================")
+print("Movies: \n  [W] Wall-E       - P150   \n  [X] X-Men        - P200\n  [Y] Yogi Bear    - P250\n  [Z] Zombieland   - P300")
+print("------------------------------")
+print("Discounts:\n  [R] Regular      - 0%\n  [V] VIP          - 10%\n  [S] Senior       - 20%")
+print("==============================")
+
+movie_code = input("Enter Movie Code (W, X, Y, Z): ")
+discount_code = input("Enter Discount Code (R, V, S): ")
+quantity = float(input("Enter how many tickets: "))
+
+match movie_code:
+    case "W":
+        price = 150
+    case "X":
+        price = 200
+    case "Y":
+        price = 250
+    case "Z":
+        price = 300
+        
+match discount_code:
+    case "R":
+        discount = 1
+    case "V":
+        discount = .9
+    case "S":
+        discount = .8
+
+def compute(movie_code,discount_code,quantity):
+    return (movie_code * discount_code) * quantity
+
+print(f"The total is P{compute(price,discount,quantity):.2f}")
