779205e77ae8b0613c95cc154afa50557b6e5da0
AdelleStuuu~ <adelledoesstuff@gmail.com>
2025-10-17 22:15:28 +0800

    lab 5 documentation

--- Quiz Stuff/Lab Activies/Lab Activity 5/Movie Ticket.py
+++ Quiz Stuff/Lab Activies/Lab Activity 5/Movie Ticket.py
@@ -6,20 +6,23 @@ print("------------------------------")
 print("Discounts:\n  [R] Regular      - 0%\n  [V] VIP          - 10%\n  [S] Senior       - 20%")
 print("==============================")
 
+#initialize the code and use inputs
 movie_code = input("Enter Movie Code (W, X, Y, Z): ")
 discount_code = input("Enter Discount Code (R, V, S): ")
 quantity = float(input("Enter how many tickets: "))
 
+# Match case converting the code to its price equivalent
 match movie_code:
     case "W":
         price = 150
-    case "X":
+    case "X": 
         price = 200
     case "Y":
         price = 250
     case "Z":
         price = 300
-        
+
+# Match case converting the code to its discount amount equivalent        
 match discount_code:
     case "R":
         discount = 1
@@ -28,6 +31,7 @@ match discount_code:
     case "S":
         discount = .8
 
+# multiply the price and and discount before multiplying quantity
 def compute(movie_code,discount_code,quantity):
     return (movie_code * discount_code) * quantity
 
--- Quiz Stuff/Lab Activies/Lab Activity 5/phrase changer.py
+++ Quiz Stuff/Lab Activies/Lab Activity 5/phrase changer.py
@@ -1,15 +1,24 @@
+# initialize the vowels
 vowel = "aeiou"
 
+# provide an input for the phrase
 phrase = input("Enter a sentence or phrase: ")
+# print the original string first 
 print(f"Original string: {phrase}")
 
+# for loop to loop through the phrase
 for i in phrase: 
+    # check if spaces are present
+    # replace the spaces with the letter x
     if i == " ":
-        phrase = phrase.replace(" ", "x")
-        
+        phrase = phrase.replace(" ", "x") 
+    # lower the letter first before checking if its in the vowel
+    # use membership in instead of doing another for loop   
     if i.lower() in vowel:
+        #make them uppercase if its a vowel
         phrase = phrase.replace(i,i.upper())
     else:
+        #make them lowercase if its a consonant
         phrase = phrase.replace(i,i.lower())
-         
+# print the entire string 
 print(f"Processed string: {phrase}")
