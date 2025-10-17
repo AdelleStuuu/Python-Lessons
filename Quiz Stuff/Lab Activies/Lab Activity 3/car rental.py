a2fbf163315aae568dd386637b756ba3fbe91f90
AdelleStuuu~ <adelledoesstuff@gmail.com>
2025-10-17 22:38:21 +0800

    lab activity 3 documentation

--- Quiz Stuff/Lab Activies/Lab Activity 3/car rental.py
+++ Quiz Stuff/Lab Activies/Lab Activity 3/car rental.py
@@ -1,15 +1,24 @@
+# promp the user to enter necessary information 
+# for the rental information
 age = float(input('Enter driver\'s age: '))
 carDailyRate = float(input('Enter the car\'s daily rate: '))
 rentalDays  = float(input('Enter rental days: '))
+# initialize variables for the fee, discount and price
 discount = .80
 fee = 1000
+# multiply the rate and rental days to get the initial price
 price = carDailyRate * rentalDays
-
+# check if the user is in the appropriate age to rent
 if age < 21: 
+    # tell the user theyre not allowed to rent 
+    # if they dont reach the required age 
     print('Only drivers aged 21 years old or above can rent.')
 else: 
+    # if the user is legal, check if theyre of senior age to apply discount
     if age >= 60:
         price *= discount
+    # add a fee if the rental days exceed or reach 30
     if rentalDays >= 30: 
         price += fee
+    # give the user the total cost
     print(f'Your total cost is ${price:.2f}.')
--- Quiz Stuff/Lab Activies/Lab Activity 3/scholarship eligibility.py
+++ Quiz Stuff/Lab Activies/Lab Activity 3/scholarship eligibility.py
@@ -1,26 +1,35 @@
+# prompt the use to enter info about their 
+# gpa, extra curricular and volunteer hours
 gpa = float(input('Enter GPA: '))
 extracurricularActivites = int(input('Enter extracurricular activity hours: '))
 volunteerHours = int(input('Enter volunteer hours: '))
+# initialize the points to start at 0
 points = 0
 
+# check if the user has added values that are not valid 
 if gpa >= 5.00 or extracurricularActivites < 0 or volunteerHours < 0:
+    # tell them that their input is invalid 
     print('An input is out of range.')
 else:
+    # check the user's grade and give them 
+    # grades depending on the gpa that they have
     if gpa == 1.00 or gpa <= 1.50:
         points += 3
     elif gpa == 1.75 or gpa <= 2.25:
         points += 2
     elif gpa == 2.50 or gpa <= 3.00:
         points += 1
-    
+    # add 10 points if the extra curricular exceeded 
+    # or reached 100 hours and add 5 if they didnt
     if extracurricularActivites >= 100:
         points += 10
     else:
         points += 5
-
+    # add 20 points if the volunteer hours exceeded 
+    # or reached 100 hours and add 10 if they didnt
     if volunteerHours >= 100:
         points += 20
     else:
         points += 10
-    
+    # print the score that the user accumulated
     print(f'Scholarship Eligibility Score is {points}.')
